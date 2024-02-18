87e580b55d964fe62769ecebd97c86be15e2fabc
BodyaSova <bod.adamenko@gmail.com>
2024-02-18 20:16:35 +0200

    step: js genplan and add Burger menu, mobile adaptive

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,30 @@
-  
+const installGenplan = () => {
+    const address = document.querySelector('#address');
+    const floor = document.querySelector('#floor');
+    const flat = document.querySelector('#flat');
+    const builds = document.querySelectorAll('.build-path');
+    
+    builds.forEach(build => {
+        build.addEventListener('mouseover', ()=> {
+            const buildAdress = build.getAttribute('data-address');
+            const buildFloor =  build.getAttribute('data-floor-quantity');
+            const buildFlat = build.getAttribute('data-flat-quantity');
+            
+            address.innerHTML = buildAdress;
+            floor.innerHTML = buildFloor;
+            flat.innerHTML = buildFlat;
+        })
+    })
+}
+
+document.querySelector('.genplan') ? installGenplan() : null;
+
+
+// Burger-Menu 
+const headerNav = document.querySelector('.header-nav');
+const btnBurger = document.querySelector('.hamburger');
+
+btnBurger.addEventListener('click', () => {
+    headerNav.classList.toggle('show');
+    btnBurger.classList.toggle('is-active');
+})
